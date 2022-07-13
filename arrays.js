121a5da60586b15b3caefd2808d5bf67fe2bb3df
Karrar Qasim <Karawr1996@gmail.com>
2022-07-13 04:19:32 -0600

    arrprac

--- arrays.js
+++ arrays.js
@@ -1,3 +1,12 @@
-var Cars = ["Pen", "Pencil", "Book," "Moon"];
-var Lamps = [1, 2, 3, 4];
+var cars = ["Pen", "Pencil", "Book,"];
+console.log(cars.unshift(1))
+//the unshift adds 1 or more elements to the begining of an array
+var lamps = [1, 2, 3, 4, 5, 6];
+console.log(lamps.slice(0, 2));
+//slice does not change the orignal array. It returns a differnt copy of elements from the original
 var Books = ["true", "flase", "yes", "no"]
+books.pop();
+console.log(books);
+var books ="true"
+console.log(books);
+//pop method returns last element and take it off.
